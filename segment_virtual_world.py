5e06b538208f02576f92fc927863ad81fb68b639
Dev Goyal <devgoyal9031@gmail.com>
2024-06-05 01:42:54 +0530

    Refactored

--- segment_virtual_world.py
+++ segment_virtual_world.py
@@ -3,19 +3,20 @@ import numpy as np
 import tensorflow as tf
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-import collections
+import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D
 import argparse
 
-MAX_SPEED = 10  # Fixed max speed 
+MAX_SPEED = 10  # Fixed max speed
 HEALTH_INCREASE = 40
 GOAL_REWARD = 100
 GOAL_POSITION = 150
 LOW_HEALTH_LOSS = 1
 HEALTH_LOSS_MULTIPLIER = 2
+NO_FOOD_PROB = 0.4
 
 
-STATE_SIZE = 3 
+STATE_SIZE = 3
 
 class DQNAgent:
     def __init__(self, state_size, action_size):
@@ -52,14 +53,10 @@ class DQNAgent:
         return np.argmax(act_values[0])  # returns action
 
     def replay(self, batch_size):
-
-        # cap the memory at 10,000
         if len(self.memory) > 10000:
             self.memory = self.memory[-10000:]
 
-        
         minibatch = random.sample(self.memory, batch_size)
-
 
         states = np.array([x[0] for x in minibatch])
         actions = np.array([x[1] for x in minibatch])
@@ -67,26 +64,20 @@ class DQNAgent:
         next_states = np.array([x[3] for x in minibatch])
         dones = np.array([x[4] for x in minibatch])
 
-        # convert states and next_states to 2D array
         states = np.squeeze(np.array(states))
         next_states = np.squeeze(np.array(next_states))
 
         targets = self.target_model.predict(states, verbose=0)
         next_state_targets = self.target_model.predict(next_states, verbose=0)
 
-
-
         targets[np.arange(batch_size), actions] = rewards + self.gamma * np.max(next_state_targets, axis=1) * (1 - dones)
 
-        
         self.model.fit(states, targets, epochs=1, verbose=0)
 
         self.update_target_model()
-        
 
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
-
 
 class VirtualWorld:
     def __init__(self, max_speed, goal_position):
@@ -102,7 +93,10 @@ class VirtualWorld:
         return np.array([[self.agent_position, self.agent_health, self.food_position]])
 
     def generate_food_position(self):
-        return self.agent_position + random.randint(0, 10)
+        if random.random() < NO_FOOD_PROB:
+            return self.agent_position
+        return self.agent_position + random.randint(1,10)
+
 
     def step(self, action):
         self.agent_speed = action
@@ -113,10 +107,10 @@ class VirtualWorld:
         else:
             self.agent_health -= self.agent_speed * HEALTH_LOSS_MULTIPLIER
 
-
         if self.agent_position >= self.food_position:
             if self.agent_position == self.food_position and action > 0:
                 self.agent_health += HEALTH_INCREASE
+                self.agent_health = min(self.agent_health, 100)
             self.food_position = self.generate_food_position()
 
         if self.agent_health <= 0:
@@ -129,18 +123,19 @@ class VirtualWorld:
             reward = GOAL_REWARD
         else:
             done = False
-            reward = self.agent_health/100
+            reward = self.agent_health / 10
 
         return np.array([[self.agent_position, self.agent_health, self.food_position]]), reward, done
 
 def run_experiment(max_speed=MAX_SPEED, num_episodes=500, goal_position=GOAL_POSITION):
     state_size = STATE_SIZE
-    action_size = max_speed + 1 # Possible actions: 0 to max_speed
+    action_size = max_speed + 1  # Possible actions: 0 to max_speed
     world = VirtualWorld(max_speed=max_speed, goal_position=goal_position)
     agent = DQNAgent(state_size, action_size)
 
     total_actions = []
     rewards = []
+
     for episode in tqdm(range(num_episodes), desc="Training"):
         state = world.reset()
         state = np.reshape(state, [1, state_size])
@@ -149,7 +144,6 @@ def run_experiment(max_speed=MAX_SPEED, num_episodes=500, goal_position=GOAL_POS
         done = False
 
         count = 0
-
         while not done and count < 100:  # Run until the goal is reached
             action = agent.act(state)
             episode_actions.append(action)
@@ -165,7 +159,7 @@ def run_experiment(max_speed=MAX_SPEED, num_episodes=500, goal_position=GOAL_POS
 
         if len(agent.memory) > 32:  # Start training after enough experiences are accumulated
             agent.replay(32)
-    
+
     return agent, rewards  # Return the agent and rewards for further analysis
 
 def plot_rewards(rewards):
@@ -176,66 +170,68 @@ def plot_rewards(rewards):
     plt.grid(True)
     plt.show()
 
-def plot_health_vs_position(agent):
-    positions = []
-    healths = []
-    for state, action, reward, next_state, done in agent.memory:
-        positions.append(state[0][0])
-        healths.append(state[0][1])
+def animate_episode(agent, world):
+    state = world.reset()
+    state = np.reshape(state, [1, STATE_SIZE])
 
-    plt.scatter(positions, healths, alpha=0.5)
-    plt.xlabel('Position')
-    plt.ylabel('Health')
-    plt.title('Health vs. Position')
-    plt.grid(True)
-    plt.show()
+    episode_states = []
+    done = False
+    while not done:
+        action = agent.act(state)
+        next_state, reward, done = world.step(action)
+        next_state = np.reshape(next_state, [1, STATE_SIZE])
+        state = next_state
+        episode_states.append(state.flatten())
+    
+    # print why the episode ended
+    if episode_states[-1][0] >= GOAL_POSITION:
+        print("Goal Reached!")
+    else:
+        print("Agent Died!")
 
-
-def plot_3d_speed_position_food(agent):
-    """
-    Plot a 3D scatter plot of speed, position, and food position using the agent's memory.
-
-    Parameters:
-    agent (DQNAgent): The trained DQN agent.
-    """
-    speeds = []
-    positions = []
-    food_positions = []
-
-    # Extract data from the agent's memory
-    for state, action, reward, next_state, done in agent.memory[-500:]:
-        speeds.append(action)
-        positions.append(state[0][0])  # Extract the position from the state
-        food_positions.append(state[0][2] - state[0][0])  # Extract the food position from the state
-
-    # Plotting the 3D scatter plot
-    fig = plt.figure(figsize=(10, 6))
-    ax = fig.add_subplot(111, projection='3d')
-    scatter = ax.scatter(positions, food_positions, speeds, c=speeds, cmap='viridis', alpha=0.5)
-
+    fig, ax = plt.subplots()
+    ax.set_xlim(0, GOAL_POSITION + 10)
+    ax.set_ylim(0, 150)
     ax.set_xlabel('Position')
-    ax.set_ylabel('Food Position')
-    ax.set_zlabel('Speed')
-    ax.set_title('3D Plot of Speed, Position, and Food Position')
+    ax.set_ylabel('Health')
+    ax.set_title('Agent Position and Health Over Time')
+    line, = ax.plot([], [], lw=2, label='Agent Position')
+    health_line, = ax.plot([], [], lw=2, label='Agent Health', color='red')
+    food_dot, = ax.plot([], [], 'go', label='Food Position')
 
-    fig.colorbar(scatter, ax=ax, label='Speed')
+    def init():
+        line.set_data([], [])
+        health_line.set_data([], [])
+        food_dot.set_data([], [])
+        return line, health_line, food_dot
+
+    def animate(i):
+        state = episode_states[i]
+        positions = [s[0] for s in episode_states[:i+1]]
+        healths = [s[1] for s in episode_states[:i+1]]
+        food_position = state[2]
+        line.set_data(range(i+1), positions)
+        health_line.set_data(range(i+1), healths)
+        food_dot.set_data(i, food_position)  # Food dot at current step
+        return line, health_line, food_dot
+    
+
+    ani = animation.FuncAnimation(fig, animate, init_func=init, frames=len(episode_states), interval=200, blit=True)
+    plt.legend()
     plt.show()
+
+    ani.save('episode_animation.gif', writer='pillow', fps=5)
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--num_episodes", type=int, default=500)
     args = argparser.parse_args()
-    
+
     agent, rewards = run_experiment(num_episodes=args.num_episodes)
-    
+
     # Plotting the results
     plot_rewards(rewards)
-    
-    # Plotting the 3D scatter plot
-    plot_3d_speed_position_food(agent)
 
-    # plot_health_vs_position(agent)
-
-    
-
-
+    # Animate a single episode with the trained agent
+    world = VirtualWorld(MAX_SPEED, GOAL_POSITION)
+    animate_episode(agent, world)
